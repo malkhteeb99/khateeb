26d1efa712eff017cae551c7c36e691f0a2dae6e
mohammadkh <malkhteeb81@gmail.com>
2023-12-08 13:46:17 +0200

    first commit

---
+++ src/main/java/com/example/ai2/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.ai2;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    @FXML
+    private Button start;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    void Go_Neural(ActionEvent event) {
+
+    }
+
+}
+
